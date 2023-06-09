4f64a249ddee144ddfd179f7461203bb8907886d
TheRoboDoc <maki.eriks@gmail.com>
2023-06-09 21:55:44 +0300

    Added supress items Suppressing messages for uneeded or breaking changes such as one that wants to change Commands method Ping to a static. That blocks allocation of commands as DSharpPlus library does not allow that

--- Motherboard/GlobalSuppressions.cs
+++ Motherboard/GlobalSuppressions.cs
@@ -5,4 +5,9 @@
 
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("Style", "IDE0090:Use 'new(...)'", Justification = "<Pending>", Scope = "member", Target = "~M:Motherboard.SlashCommands.Intro(DSharpPlus.SlashCommands.InteractionContext)~System.Threading.Tasks.Task")]
+[assembly: SuppressMessage("Style", "IDE0090:Use 'new(...)'", Justification = "Style choice I want to keep", Scope = "module")]
+[assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Will break the handler if applied", Scope = "member", Target = "~M:Motherboard.Response.Handler.Run(DSharpPlus.DiscordClient,DSharpPlus.EventArgs.MessageCreateEventArgs)~System.Threading.Tasks.Task")]
+[assembly: SuppressMessage("Usage", "CA2211:Non-constant fields should not be visible", Justification = "Will break if applied", Scope = "member", Target = "~F:Motherboard.Program.botClient")]
+[assembly: SuppressMessage("Usage", "CA2211:Non-constant fields should not be visible", Justification = "Will break if applied", Scope = "member", Target = "~F:Motherboard.Program.openAiService")]
+[assembly: SuppressMessage("Style", "IDE0057:Use range operator", Justification = "No", Scope = "member", Target = "~M:Motherboard.Program.MainAsync~System.Threading.Tasks.Task")]
+[assembly: SuppressMessage("Style", "IDE0057:Use range operator", Justification = "No", Scope = "member", Target = "~M:Motherboard.Command.SlashCommands.Commands(DSharpPlus.SlashCommands.InteractionContext)~System.Threading.Tasks.Task")]
