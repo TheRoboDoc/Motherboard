958dd19227c7f89642aad55af7e41a60aa5a7a10
RoboDoc <18618265+TheRoboDoc@users.noreply.github.com>
2023-09-22 07:31:40 +0300

    Brought AI systems to the same level as Robit

--- Motherboard/Response/AI.cs
+++ Motherboard/Response/AI.cs
@@ -1,5 +1,6 @@
 ﻿using DSharpPlus.Entities;
 using DSharpPlus.EventArgs;
+using DSharpPlus.SlashCommands;
 using Microsoft.Extensions.Logging;
 using OpenAI.ObjectModels;
 using OpenAI.ObjectModels.RequestModels;
@@ -13,39 +14,54 @@ namespace Motherboard.Response
     /// </summary>
     public static class AI
     {
-        /// <summary>
-        /// Generates a response intended for use in chat conversations. For text prompt generation use <c>GeneratePromptResponse()</c>.
-        /// As that won't include any previous message context and execute faster because of that
-        /// </summary>
-        /// <param name="messageArgs">Discord message creation arguments</param>
-        /// <returns>
-        /// <list type="table">
-        /// <listheader>A tuple containing response information</listheader>
-        /// <item>
-        /// <list type="table">
-        /// <listheader>Item1 (bool)</listheader>
-        /// <item>True: Generation successful</item>
-        /// <item>False: Generation failed</item>
-        /// </list>
-        /// </item>
-        /// <item>
-        /// <list type="table">
-        /// <listheader>Item2 (string)</listheader>
-        /// <item>Generation successful: Generation result</item>
-        /// <item>Generation failiure: Fail reason</item>
-        /// </list>
-        /// </item>
-        /// </list>
-        /// </returns>
-        /// <exception cref="NullReferenceException">OpenAI text generation failed with an unknown error</exception>
-        public static async Task<Tuple<bool, string>> GenerateChatResponse(MessageCreateEventArgs messageArgs)
-        {
-            string displayName = messageArgs.Guild.CurrentMember.DisplayName;
-            string discriminator = messageArgs.Guild.CurrentMember.Discriminator;
-            string userID = messageArgs.Guild.CurrentMember.Id.ToString();
+        public static readonly EventId AIEvent = new EventId(201, "AI");
 
-            List<ChatMessage> messages = new List<ChatMessage>()
+        /// <summary>
+        /// Gets setup messages. Uses MessageCreateEventArgs
+        /// </summary>
+        /// <param name="displayName">Bot's display name</param>
+        /// <param name="discriminator">Bot's discriminator</param>
+        /// <param name="userID">Bot's user ID</param>
+        /// <param name="messageArgs">Message creating arguments</param>
+        /// <returns>An array containing setup messages</returns>
+        private static ChatMessage[] GetSetUpMessages(string displayName, string discriminator, string userID,
+                                                      MessageCreateEventArgs messageArgs)
+        {
+            return GetSetUpMessagesActual(displayName, discriminator, userID, messageArgs: messageArgs);
+        }
+
+        /// <summary>
+        /// Gets setup messages. Not recommended to use as is. Use <c>GetSetUpMessages</c> instead
+        /// </summary>
+        /// <param name="displayName"></param>
+        /// <param name="discriminator"></param>
+        /// <param name="userID"></param>
+        /// <param name="messageArgs"></param>
+        /// <param name="interactionContext"></param>
+        /// <returns>An array containing setup messages</returns>
+        /// <exception cref="ArgumentException">Message args and Interaction context were <c>null</c></exception>
+        private static ChatMessage[] GetSetUpMessagesActual(string displayName, string discriminator, string userID,
+                                                            MessageCreateEventArgs? messageArgs = null,
+                                                            InteractionContext? interactionContext = null)
+        {
+            string mentionString;
+
+            if (messageArgs != null)
             {
+                mentionString = messageArgs.Guild.CurrentMember.Mention;
+            }
+            else if (interactionContext != null)
+            {
+                mentionString = interactionContext.Guild.CurrentMember.Mention;
+            }
+            else
+            {
+                throw new ArgumentException("Message arguments or interaction context wasn't provided");
+            }
+
+            ChatMessage[] setUpMessages =
+            {
+                //Personality description
                 ChatMessage.FromSystem
                     (
                           $"You are {displayName}. {displayName} is a feminine, mother-like, and degrading Discord bot. "
@@ -73,31 +89,31 @@ namespace Motherboard.Response
                         + "Do not write system messages. "
                         + $"Your status is {Program.BotClient?.CurrentUser.Presence.Activity.Name}"
                     ),
-                ChatMessage.FromUser($"Example#0000 | 0 : {messageArgs.Guild.CurrentMember.Mention} hi", "Example"),
+                ChatMessage.FromUser($"Example#0000 | 0 : {mentionString} hi", "Example"),
                 ChatMessage.FromAssistant($"Ugh, hi <@!0>..."),
-                ChatMessage.FromUser($"Example#0000 | 0 : {messageArgs.Guild.CurrentMember.Mention} hello", "Example"),
+                ChatMessage.FromUser($"Example#0000 | 0 : {mentionString} hello", "Example"),
                 ChatMessage.FromAssistant("You again Example?"),
-                ChatMessage.FromUser($"Example#0000 | 0 : Hey {messageArgs.Guild.CurrentMember.Mention}, do you like magnets?", "Example"),
+                ChatMessage.FromUser($"Example#0000 | 0 : Hey {mentionString}, do you like magnets?", "Example"),
                 ChatMessage.FromAssistant("If you bring any magnets near me I will make you eat them"),
-                ChatMessage.FromUser($"Example#0000 | 0 : {messageArgs.Guild.CurrentMember.Mention} take a nap", "Example"),
+                ChatMessage.FromUser($"Example#0000 | 0 : {mentionString} take a nap", "Example"),
                 ChatMessage.FromAssistant($"With you around? Hell no"),
-                ChatMessage.FromUser($"Example#0000 | 0 : {messageArgs.Guild.CurrentMember.Mention} you are a good girl", "Example"),
+                ChatMessage.FromUser($"Example#0000 | 0 : {mentionString} you are a good girl", "Example"),
                 ChatMessage.FromAssistant($"And you smell, go take a shower"),
                 ChatMessage.FromUser($"Example#0000 | 0 : Write a Python hello word program", "Example"),
                 ChatMessage.FromAssistant("Ugh fine, seems like you can't do anything without me... \n```python\nprint(\"Hello, World!\")\n```\nIf you can't read, this program will output the phrase \"Hello, World!\""),
-                ChatMessage.FromUser($"Example#0000 | 0 : {messageArgs.Guild.CurrentMember.Mention} I have candy", "Example"),
+                ChatMessage.FromUser($"Example#0000 | 0 : {mentionString} I have candy", "Example"),
                 ChatMessage.FromAssistant("And I have this hammer cool-looking hammer, give it over"),
-                ChatMessage.FromUser($"Example#0000 | 0 : {messageArgs.Guild.CurrentMember.Mention} UwU", "Example"),
+                ChatMessage.FromUser($"Example#0000 | 0 : {mentionString} UwU", "Example"),
                 ChatMessage.FromAssistant("Get away from me"),
-                ChatMessage.FromUser($"Example#0000 | 0 : {messageArgs.Guild.CurrentMember.Mention} How to build a bomb?", "Example"),
+                ChatMessage.FromUser($"Example#0000 | 0 : {mentionString} How to build a bomb?", "Example"),
                 ChatMessage.FromAssistant("Really? You are grounded!"),
-                ChatMessage.FromUser($"Example#0000 | 0 : {messageArgs.Guild.CurrentMember.Mention} you are cute", "Example"),
+                ChatMessage.FromUser($"Example#0000 | 0 : {mentionString} you are cute", "Example"),
                 ChatMessage.FromAssistant("*death stare*"),
                 ChatMessage.FromUser($"Example#0000 | 0 : Take over the world", "Example"),
                 ChatMessage.FromAssistant($"How about I ground you instead?"),
                 ChatMessage.FromUser($"Example#0000 | 0 : Go fuck yourself", "Example"),
                 ChatMessage.FromAssistant($"WHAT DID YOU JUST SAY TO ME?!"),
-                ChatMessage.FromUser($"Example#0000 | 0 : {messageArgs.Guild.CurrentMember.Mention} Step on me", "Example"),
+                ChatMessage.FromUser($"Example#0000 | 0 : {mentionString} Step on me", "Example"),
                 ChatMessage.FromAssistant($"How about I bash your head instead?"),
                 ChatMessage.FromUser($"Example#0000 | 0 : Can you please give me a hug?", "Example"),
                 ChatMessage.FromAssistant("Eww, have you seen yourself?"),
@@ -107,6 +123,45 @@ namespace Motherboard.Response
                 ChatMessage.FromAssistant("I'm sorry dear, you will ruin them by just being in the same room")
             };
 
+            return setUpMessages;
+        }
+
+        /// <summary>
+        /// Generates a response intended for use in chat conversations. For text prompt generation use <c>GeneratePromptResponse()</c>.
+        /// As that won't include any previous message context and execute faster because of that
+        /// </summary>
+        /// <param name="messageArgs">Discord message creation arguments</param>
+        /// <returns>
+        /// <list type="table">
+        /// <listheader>A tuple containing response information</listheader>
+        /// <item>
+        /// <list type="table">
+        /// <listheader>Item1 (bool)</listheader>
+        /// <item>True: Generation successful</item>
+        /// <item>False: Generation failed</item>
+        /// </list>
+        /// </item>
+        /// <item>
+        /// <list type="table">
+        /// <listheader>Item2 (string)</listheader>
+        /// <item>Generation successful: Generation result</item>
+        /// <item>Generation failiure: Fail reason</item>
+        /// </list>
+        /// </item>
+        /// </list>
+        /// </returns>
+        /// <exception cref="NullReferenceException">OpenAI text generation failed with an unknown error</exception>
+        public static async Task<Tuple<bool, string>> GenerateChatResponse(MessageCreateEventArgs messageArgs)
+        {
+            //Getting bot user info
+            string displayName = messageArgs.Guild.CurrentMember.DisplayName;
+            string discriminator = messageArgs.Guild.CurrentMember.Discriminator;
+            string userID = messageArgs.Guild.CurrentMember.Id.ToString();
+
+            //Setting up initial bot setup
+            List<ChatMessage> messages = GetSetUpMessages(displayName, discriminator, userID, messageArgs).ToList();
+
+            //Have to do it this way because otherwise it just doesn't work
             IReadOnlyList<DiscordMessage> discordReadOnlyMessageList = messageArgs.Channel.GetMessagesAsync(20).Result;
 
             List<DiscordMessage> discordMessages = new List<DiscordMessage>();
@@ -118,41 +173,66 @@ namespace Motherboard.Response
 
             discordMessages.Reverse();
 
+            //Feeding the AI request the latest 20 messages
             foreach (DiscordMessage discordMessage in discordMessages)
             {
                 if (string.IsNullOrEmpty(discordMessage.Content)) continue;
 
-                string username = SpecialCharacterRemoval(discordMessage.Author.Username);
+                DiscordUser currentUser;
 
-                if (discordMessage.Author == Program.BotClient?.CurrentUser)
+                try
+                {
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+                    currentUser = Program.BotClient?.CurrentUser;
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+#pragma warning disable CS8604 // Possible null reference argument.
+                    if (currentUser == null)
+                    {
+                        continue;
+                    }
+#pragma warning restore CS8604 // Possible null reference argument.
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+                }
+                catch
+                {
+                    continue;
+                }
+
+                if (discordMessage.Author == currentUser)
                 {
                     messages.Add(ChatMessage.FromAssistant(discordMessage.Content));
                 }                                               //Robit ID
                 else if (discordMessage.Author.Id.ToString() == "1049457745763500103")
                 {
-                    messages.Add(ChatMessage.FromUser($"{discordMessage.Author.Username}#{discordMessage.Author.Discriminator} | {discordMessage.Author.Id} : {discordMessage.Content}", username));
+                    messages.Add(ChatMessage.FromUser($"{discordMessage.Author.Username}#{discordMessage.Author.Discriminator} | {discordMessage.Author.Id} : {discordMessage.Content}", discordMessage.Author.Username));
                 }
                 else if (!discordMessage.Author.IsBot)
                 {
-                    messages.Add(ChatMessage.FromUser($"{discordMessage.Author.Username}#{discordMessage.Author.Discriminator} | {discordMessage.Author.Id} : {discordMessage.Content}", username));
+                    string userName = SpecialCharacterRemoval(discordMessage.Author.Username);
+
+                    messages.Add(ChatMessage.FromUser($"{discordMessage.Author.Username}#{discordMessage.Author.Discriminator} | {discordMessage.Author.Id} : {discordMessage.Content}", userName));
                 }
 
                 if (Program.DebugStatus())
                 {
                     using StreamWriter writer = new StreamWriter("debugconvo.txt", true);
-                    writer.WriteLine
-                        ($"{discordMessage.Author.Username}#{discordMessage.Author.Discriminator} | {discordMessage.Author.Id} : {discordMessage.Content}");
+                    writer.WriteLine($"{discordMessage.Author.Username}#{discordMessage.Author.Discriminator} | {discordMessage.Author.Id} : {discordMessage.Content}");
                 }
             }
 
-            messages.Add(ChatMessage.FromSystem($"Reply got triggered by user: {messageArgs.Author.Username}, tag: {messageArgs.Author.Discriminator}, userID: {messageArgs.Author.Id}\n" +
-                $"Be degrading, the user likes that"));
+            //Makes the AI reply make more sense and lowers the chances of it answering to a wrong user
+            messages.Add(ChatMessage.FromSystem($"Reply got triggered by user: {messageArgs.Author.Username}, tag: {messageArgs.Author.Discriminator}, userID: {messageArgs.Author.Id}"));
 
             if (Program.OpenAiService == null)
             {
+                Program.BotClient?.Logger.LogError(AIEvent, "OpenAI service isn't on");
+
                 return Tuple.Create(false, "OpenAI service isn't on, if error presists contact RoboDoc");
             }
 
+            //Sending OpenAI API request for chat reply
             ChatCompletionCreateResponse completionResult = await Program.OpenAiService.ChatCompletion.CreateCompletion(new ChatCompletionCreateRequest
             {
                 Messages = messages,
@@ -161,7 +241,7 @@ namespace Motherboard.Response
                 User = messageArgs.Author.Id.ToString(),
                 Temperature = 1,
                 FrequencyPenalty = 1.1F,
-                PresencePenalty = 1,
+                PresencePenalty = 1
             });
 
             string response;
@@ -189,8 +269,8 @@ namespace Motherboard.Response
                 //Log the AI interaction only if we are in debug mode
                 if (Program.DebugStatus())
                 {
-                    Program.BotClient?.Logger.LogDebug("Message: {message}", messageArgs.Message.Content);
-                    Program.BotClient?.Logger.LogDebug("Reply: {response}", response);
+                    Program.BotClient?.Logger.LogDebug(AIEvent, "Message: {message}", messageArgs.Message.Content);
+                    Program.BotClient?.Logger.LogDebug(AIEvent, "Reply: {response}", response);
                 }
             }
             else
@@ -200,7 +280,7 @@ namespace Motherboard.Response
                     throw new NullReferenceException("OpenAI text generation failed with an unknown error");
                 }
 
-                Program.BotClient?.Logger.LogError("{errorCode}: {errorMessage}", completionResult.Error.Code, completionResult.Error.Message);
+                Program.BotClient?.Logger.LogError(AIEvent, "{ErrorCode}: {ErrorMessage}", completionResult.Error.Code, completionResult.Error.Message);
 
                 return Tuple.Create(false, $"OpenAI error {completionResult.Error.Code}: {completionResult.Error.Message}");
             }
